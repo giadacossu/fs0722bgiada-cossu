51c3c406d586e0e10758aa28d96978daf210406c
giadacossu <giadacossu2000@gmail.com>
2022-10-31 15:56:21 +0100

    d1

---
+++ m1/w2/d1/esercitazione/js/index.js
@@ -0,0 +1,10 @@
+alert('Usa il pop up per dare dei messaggi');
+window.alert('Sto studiando JS');
+
+var anni = prompt('quanti anni hai?')
+if(anni >= 18){
+    document.write(' ')
+} else if(anni <= 10){
+    location.href = 'https://www.gioco.it/'
+}
+
---
+++ m1/w2/d1/spiegazione/sito1/js/script.js
@@ -0,0 +1,2 @@
+//alert("hello world")
+document.getElementById("test").innerHTML="hello world"
