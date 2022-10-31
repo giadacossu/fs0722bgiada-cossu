e41c3441359fa45a27be58f8c2b16d7657feb664
giadacossu <giadacossu2000@gmail.com>
2022-10-31 16:43:03 +0100

    d1

--- m1/w2/d1/esercitazione/js/index.js
+++ m1/w2/d1/esercitazione/js/index.js
@@ -1,6 +1,10 @@
 alert('Usa il pop up per dare dei messaggi');
 window.alert('Sto studiando JS');
 
+var nomeGiocatore = window.prompt( 'inserisci il tuo nome')
+document.getElementById('titolo').innerHTML = 'benvenuto ' + nomeGiocatore
+
+
 var anni = prompt('quanti anni hai?')
 if(anni >= 18){
     document.write(' ')
