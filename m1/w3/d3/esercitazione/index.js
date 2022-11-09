5bbeb7a9810b81e6e49045b433a9a5bbe5904900
giadacossu <giadacossu2000@gmail.com>
2022-11-09 22:17:47 +0100

    d3

--- m1/w3/d3/esercitazione/index.js
+++ m1/w3/d3/esercitazione/index.js
@@ -1,6 +1,6 @@
 document.querySelector('#salva').addEventListener('click',function(){
 
-
+if( testo = document.querySelector('#text').value != ''){
     let testo = document.querySelector('#text').value;
 
     let div = document.createElement('div');
@@ -9,11 +9,31 @@ document.querySelector('#salva').addEventListener('click',function(){
 
     let fatto = document.createElement('button')
     fatto.textContent = 'fatto'
+  fatto.classList.add('aggiuntibottone')
+
+  let remove = document.createElement('button')
+  remove.textContent = 'remove'
+  remove.classList.add('aggiuntibottone')
 
 
-div.append(fatto)
+  div.append(fatto)
+div.append(remove)
+
 document.querySelector('#lista').append(div)
 
 
+fatto.addEventListener('click', function(){
+    div.classList.add('Agreen')
+ })
+   
+ 
+ remove.addEventListener('click', function(){
+        div.remove().value
+
 })
-document.querySelector('')
+testo = document.querySelector('#text').value = ''
+    }
+   
+})
+
+
