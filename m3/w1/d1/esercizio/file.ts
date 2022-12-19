b9f462ed4b58df43c3ef4183c779067327d83bfd
giadacossu <giadacossu2000@gmail.com>
2022-12-19 17:21:12 +0100

    d1

--- m3/w1/d1/esercizio/file.ts
+++ m3/w1/d1/esercizio/file.ts
@@ -7,48 +7,37 @@ let numero2 = document.getElementById('secondop')
 let bottone1 = document.getElementById('bottone1')
 let bottone2 = document.getElementById('bottone2')*/
 
-
-
-
-
 let utente1 = {
-    nome: 'marco ',
-    numero: 26
-}
-console.log(utente1.nome + utente1.numero)
+  nome: "marco ",
+  numero: 26,
+};
+console.log(utente1.nome + utente1.numero);
 
 let utente2 = {
-    nome: 'piero ',
-    numero: 77
+  nome: "piero ",
+  numero: 77,
+};
+console.log(utente2.nome + utente2.numero);
+
+function calcolo(){
+  let random = Math.floor(Math.random() * 100);
+
+console.log("numero vincente " + random);
+
+let primoRisultato = Math.abs(random - utente1.numero);
+let secondoRisultato = Math.abs(random - utente2.numero);
+
+console.log(primoRisultato);
+
+if (random === utente1.numero) {
+  console.log(utente1.nome + "hai vinto ");
+} else if (random === utente2.numero) {
+  console.log(utente2.nome + "hai vinto ");
+} else if (primoRisultato < secondoRisultato) {
+  console.log("non avete vinto ma " + utente1.nome + "è arrivato più vicino ");
+} else if (primoRisultato > secondoRisultato) {
+  console.log("non avete vinto ma " + utente2.nome + "è arrivato più vicino ");
 }
-console.log(utente2.nome + utente2.numero)
 
-
-let random = (Math.floor(Math.random() * 100))
-
-console.log('numero vincente '+ random)
-
-
- let primoRisultato =  (random - utente1.numero) 
-let secondoRisultato = (random - utente2.numero)
-
-console.log(primoRisultato)
-
-
-
-
-
- if((random  - utente1.numero) == 0) {
-  console.log(utente1.nome + 'hai vinto ' )
-   } else if((random -utente2.numero) == 0){
-    console.log( utente2.nome + 'hai vinto ' )
-   }else if (primoRisultato < secondoRisultato){
-console.log ('non avete vinto ma ' + utente1.nome + 'è arrivato più vicino ')
-   } else if (primoRisultato > secondoRisultato){
-    console.log('non avete vinto ma ' + utente2.nome + 'è arrivato più vicino ')
-   }
-
-
-
-
-
+}
+calcolo()
