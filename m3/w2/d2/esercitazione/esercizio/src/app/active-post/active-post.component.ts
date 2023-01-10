b12ed153b5d169da9f62b654098d878daf399717
giadacossu <giadacossu2000@gmail.com>
2023-01-10 22:52:18 +0100

    interface service e ts

--- m3/w2/d2/esercitazione/esercizio/src/app/active-post/active-post.component.ts
+++ m3/w2/d2/esercitazione/esercizio/src/app/active-post/active-post.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Post } from '../interface/post';
+import { PostService } from '../service/post.service';
 @Component({
   selector: 'app-active-post',
   templateUrl: './active-post.component.html',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActivePostComponent implements OnInit {
 
-  constructor() { }
+  posts:Post[] = this.postServ.getPostActive();
+  constructor(private postServ:PostService) { }
 
   ngOnInit(): void {
   }
